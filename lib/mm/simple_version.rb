ec2146272b795a440fe0695ee439fd44f651f9b5
Matt McMahand <matt@invalid8.com>
2022-03-12 13:54:35 -0500

    first commmit

---
+++ lib/mm/simple_version.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "simple_version/version"
+
+module Mm
+  module SimpleVersion
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
